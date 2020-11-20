31704c502a09ea865d543be804592501b56ddad6
Jozef Podlecki <jozef.witold.podlecki@gmail.com>
2020-11-20 17:41:59 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import { Route, Switch } from "react-router";
+
+const App: FunctionComponent = () => {
+    return <div></div>
+};
+
+export default App;
